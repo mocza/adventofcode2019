0c3e6c67fe1b4292b0e6b81757937b5ce855ae84
MOCHA <zsolt.mocza@gmail.com>
2019-12-15 22:45:49 +0000

    day3 part1 hung

--- day3.js
+++ day3.js
@@ -1,11 +1,26 @@
 "use strict";
 
-// exports.line = class Line extends Array {
-//     constructor(startCoord, endCoord) {
-//         super(startCoord, endCoord);
-//         Object.freeze(this);
-//     }
-// }
+class Line {
+    constructor(coordBegin, coordEnd) {
+        this.coords = coordRange(coordBegin, coordEnd);
+    }
+
+    has(coord) {
+        return this.coords.filter(coordInThisLine => coordInThisLine.x !== 0 && coordInThisLine.y !== 0).filter(coordInThisLine => JSON.stringify(coordInThisLine) === JSON.stringify(coord));
+    }
+
+    intersects(otherLine) {
+        // return this.coords.filter(coord => otherLine.has(coord));
+        let result = [];
+        for(let point of this.coords) {
+            let commonPoints = otherLine.has(point);
+            if(commonPoints.length !== 0) {
+                result = result.concat(commonPoints);
+            }
+        }
+        return result;
+    }
+}
 
 class Coord2 {
     constructor(x, y) {
@@ -14,33 +29,69 @@ class Coord2 {
     }    
 }
 
-class Coord extends Array {
-    constructor(x, y) {
-        super(x, y);
-        Object.freeze(this);
-    }
-
-    // get x() {
-    //     const [x, y] = this;
-    //     return this.x;
-    // }
-    // get y() {
-    //     const [x, y] = this;
-    //     return this.y;
-    // }
-    
-}
-
-function movesTo(moves, initialCoord) {
-    let result = [initialCoord];
-    let currentCoord = initialCoord;
-    for(let move of moves) {
-        result.push(moveTo(move, currentCoord));
+function intRange(start, end) {
+    let result = [];
+    if(end > start) {
+        for (let i = start; i <= end; i++) {
+            result.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i--) {
+            result.push(i);
+        }
     }
     return result;
 }
 
-function moveTo(move, currentCoord) {
+function coordRange(coordBegin, coordEnd) {
+    if(coordBegin.x === coordEnd.x) {
+        return intRange(coordBegin.y, coordEnd.y).map(nextY => new Coord2(coordBegin.x, nextY));
+    } else {
+        return intRange(coordBegin.x, coordEnd.x).map(nextX => new Coord2(nextX, coordBegin.y));
+    }
+}
+
+function intersections(lines1, lines2) {
+    let result = [];
+    for (let line1 of lines1) {
+        for (let line2 of lines2) {
+            let intersections = line1.intersects(line2);
+            if (intersections !== undefined) {
+                result = result.concat(intersections);
+            }
+        }
+    }
+    return result;
+}
+
+// class Coord extends Array {
+//     constructor(x, y) {
+//         super(x, y);
+//         Object.freeze(this);
+//     }
+
+//     // get x() {
+//     //     const [x, y] = this;
+//     //     return this.x;
+//     // }
+//     // get y() {
+//     //     const [x, y] = this;
+//     //     return this.y;
+//     // }
+    
+// }
+
+function movesToCoordinates(moves, initialCoord) {
+    let result = [initialCoord];
+    let currentCoord = initialCoord;
+    for(let move of moves) {        
+        currentCoord = moveToCoordinates(move, currentCoord);
+        result.push(currentCoord);
+    }
+    return result;
+}
+
+function moveToCoordinates(move, currentCoord) {
     const direction = move.substring(0, 1);
     const length = parseInt(move.substring(1), 10);    
     // const [currentX, currentY] = currentCoord;
@@ -56,10 +107,52 @@ function moveTo(move, currentCoord) {
     }    
 }
 
-// console.log(moveTo('D2', new Coord(0, 0)));
+function readInput(inputFile) {
+    let inputLines = [];
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(inputFile)
+      });
+      
+      lineReader.on('line', function (line) {          
+        // console.log('Line from file:', line.toString().split(','));
+        inputLines = inputLines.concat(line.toString().split(','));
+      });
+    return inputLines;
+}
 
-// exports.movesToCoordiantes = function (moves) {
-//     return moves.map(move => );
-// }
+function readInputSync(inputFile) {
+    const fs = require('fs');    
+    const inputLines = fs.readFileSync(inputFile, 'utf8').toString().split("\n");
+    return [inputLines[0].toString().split(','), inputLines[1].toString().split(',')];
+}
 
-module.exports = {moveTo, movesTo, Coord2};
+function pointsToLines(points) {
+    let result = [];
+    let prevPoint = null;
+    for (let point of points) {
+        if (prevPoint != null) {
+            result.push(new Line(prevPoint, point));
+        } 
+        prevPoint = point;        
+    }
+    return result;
+}
+
+function manhattanDistanceForClosestIntersection(intersectionPoints) {
+    let result = Number.MAX_VALUE;
+    for(let intersectionPoint of intersectionPoints) {
+        let distance = Math.abs(intersectionPoint.x) + Math.abs(intersectionPoint.y);
+        if(distance < result) {
+            result = distance;
+        }
+    }
+    return result;
+}
+
+let [inputLine1, inputLine2] = readInputSync('./inputs/day3-input.txt');
+// console.log(movesToCoordinates(inputLine1, new Coord2(0,0)));
+// console.log(pointsToLines(movesToCoordinates(inputLine1, new Coord2(0,0))));
+// console.log(pointsToLines(movesToCoordinates(inputLine2, new Coord2(0,0))));
+console.log(manhattanDistanceForClosestIntersection(intersections(pointsToLines(movesToCoordinates(inputLine1, new Coord2(0,0))), pointsToLines(movesToCoordinates(inputLine2, new Coord2(0,0))))));
+
+module.exports = {moveToCoordinates, movesToCoordinates, Coord2, intRange, coordRange, Line, intersections, pointsToLines};
--- test/day3-test.js
+++ test/day3-test.js
@@ -11,32 +11,110 @@ const assert = require('assert');
 
 // });
 
-describe('moveTo', function() {
+describe('moveToCoordinates', function() {
     it('moveLeft', function() {
-        assert.deepStrictEqual(day3.moveTo('L1', new day3.Coord2(0,0)), new day3.Coord2(-1,0));
+        assert.deepStrictEqual(day3.moveToCoordinates('L1', new day3.Coord2(0,0)), new day3.Coord2(-1,0));
     });     
     it('moveRight', function() {
-        assert.deepStrictEqual(day3.moveTo('R1', new day3.Coord2(0,0)), new day3.Coord2(1,0));
+        assert.deepStrictEqual(day3.moveToCoordinates('R1', new day3.Coord2(0,0)), new day3.Coord2(1,0));
     });     
     it('moveUp', function() {
-        assert.deepStrictEqual(day3.moveTo('U1', new day3.Coord2(0,0)), new day3.Coord2(0,1));
+        assert.deepStrictEqual(day3.moveToCoordinates('U1', new day3.Coord2(0,0)), new day3.Coord2(0,1));
     });     
     it('moveDown', function() {
-        assert.deepStrictEqual(day3.moveTo('D1', new day3.Coord2(0,0)), new day3.Coord2(0,-1));
+        assert.deepStrictEqual(day3.moveToCoordinates('D1', new day3.Coord2(0,0)), new day3.Coord2(0,-1));
     });     
 });
 
-describe('movesTo', function() {
+describe('movesToCoordinates', function() {
     it('movesOnce', function() {
-        assert.deepStrictEqual(day3.movesTo(['L1'], new day3.Coord2(0,0)), [new day3.Coord2(0,0), new day3.Coord2(-1,0)]);
+        assert.deepStrictEqual(day3.movesToCoordinates(['L1'], new day3.Coord2(0,0)), [new day3.Coord2(0,0), new day3.Coord2(-1,0)]);
     });     
     it('movesTwice', function() {
-        assert.deepStrictEqual(day3.movesTo(['L1', 'U1'], new day3.Coord2(0,0)), [new day3.Coord2(0,0), new day3.Coord2(-1,0), new day3.Coord2(-1,1)]);
+        assert.deepStrictEqual(day3.movesToCoordinates(['L1', 'U1'], new day3.Coord2(0,0)), [new day3.Coord2(0,0), new day3.Coord2(-1,0), new day3.Coord2(-1,1)]);
     });     
-    // it('moveUp', function() {
-    //     assert.deepStrictEqual(day3.moveTo('U1', new day3.Coord2(0,0)), new day3.Coord2(0,1));
-    // });     
-    // it('moveDown', function() {
-    //     assert.deepStrictEqual(day3.moveTo('D1', new day3.Coord2(0,0)), new day3.Coord2(0,-1));
-    // });     
 });
+
+describe('pointsToLines', function() {
+    it('two points to one line one length', function() {
+        assert.deepStrictEqual(day3.pointsToLines([new day3.Coord2(0,0), new day3.Coord2(-1,0)]), [new day3.Line(new day3.Coord2(0,0), new day3.Coord2(-1,0))]);
+    });     
+    it('two points to one line two length', function() {
+        assert.deepStrictEqual(day3.pointsToLines([new day3.Coord2(0,0), new day3.Coord2(2,0)]), [new day3.Line(new day3.Coord2(0,0), new day3.Coord2(2,0))]);
+    });     
+});
+
+describe('intRange', function() {
+    it('0 to 5 inclusive', function() {
+        assert.deepStrictEqual(day3.intRange(0, 5), [0, 1, 2, 3, 4, 5]);
+    });     
+    it('3 to 0 inclusive', function() {
+        assert.deepStrictEqual(day3.intRange(3, 0), [3, 2, 1, 0]);
+    });     
+});
+
+describe('coordRange', function() {
+    it('single point line', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(0,0), new day3.Coord2(0,0)), [new day3.Coord2(0,0)]);
+    });         
+    it('two points line', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(0,0), new day3.Coord2(0,1)), [new day3.Coord2(0,0), new day3.Coord2(0,1)]);
+    });     
+    it('three points line vertical', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(0,0), new day3.Coord2(0,2)), [new day3.Coord2(0,0), new day3.Coord2(0,1), new day3.Coord2(0,2)]);
+    });     
+    it('three points line horizontal', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(0,0), new day3.Coord2(2,0)), [new day3.Coord2(0,0), new day3.Coord2(1,0), new day3.Coord2(2,0)]);
+    });     
+    it('three points line horizontal going right to left', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(2,0), new day3.Coord2(0,0)), [new day3.Coord2(2,0), new day3.Coord2(1,0), new day3.Coord2(0,0)]);
+    });     
+    it('three points line vertical going from up to down', function() {
+        assert.deepStrictEqual(day3.coordRange(new day3.Coord2(0,2), new day3.Coord2(0,0)), [new day3.Coord2(0,2), new day3.Coord2(0,1), new day3.Coord2(0,0)]);
+    });     
+});
+
+describe('line', function() {    
+    it('point is not on the line ', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).has(new day3.Coord2(1,1)), []);
+    });         
+    it('point is on the line#1', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).has(new day3.Coord2(0,0)), [new day3.Coord2(0,0)]);
+    });         
+    it('point is on the line#2', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).has(new day3.Coord2(1,0)), [new day3.Coord2(1,0)]);
+    });         
+    it('lines do not intersect ', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).intersects(
+            new day3.Line(new day3.Coord2(8,8), new day3.Coord2(8,9))), []);
+    });         
+    it('lines intersect at one point #1', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).intersects(
+            new day3.Line(new day3.Coord2(-1,0), new day3.Coord2(0,0))), [new day3.Coord2(0,0)]);
+    });         
+    it('lines intersect at one point #2', function() {
+        assert.deepStrictEqual(new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0)).intersects(
+            new day3.Line(new day3.Coord2(1,1), new day3.Coord2(1,0))), [new day3.Coord2(1,0)]);
+    });         
+});
+
+describe('intersections', function() {
+    it('no intersection', function() {
+        assert.deepStrictEqual(day3.intersections([new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))], 
+        [new day3.Line(new day3.Coord2(1,1), new day3.Coord2(1,2))]), []);
+    });         
+    it('one intersection point', function() {
+        assert.deepStrictEqual(day3.intersections([new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))], 
+        [new day3.Line(new day3.Coord2(1,1), new day3.Coord2(1,0))]), [new day3.Coord2(1,0)]);
+    });         
+    it('two intersection points', function() {
+        assert.deepStrictEqual(day3.intersections([new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))], 
+        [new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))]), [new day3.Coord2(0,0), new day3.Coord2(1,0)]);
+    });         
+    it('two intersection points', function() {
+        assert.deepStrictEqual(day3.intersections([new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))], 
+        [new day3.Line(new day3.Coord2(0,0), new day3.Coord2(1,0))]), [new day3.Coord2(0,0), new day3.Coord2(1,0)]);
+    });         
+
+});
+

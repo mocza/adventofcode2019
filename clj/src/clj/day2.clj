dac69867d4a0790a26a9e2d8cf6b4e531e7efa5c
MOCHA <zsolt.mocza@gmail.com>
2020-04-26 22:35:40 +0100

    input 1202 correction

--- clj/src/clj/day2.clj
+++ clj/src/clj/day2.clj
@@ -15,7 +15,7 @@
         (into (conj before-sub result) after-sub) )
     )
       
-(defn exec-program "Executes a program of intcode instructions till halt opcode" 
+(defn exec-program "Executes intcode program of 4 lenght instructions till halt opcode" 
   [input pos]
   (if (= (get input pos) 99) 
     input
@@ -23,6 +23,10 @@
 )
 
 (def input (vec (read-input "/media/mocha/massData1/dev/adventOfCode2019/inputs/day2-input.txt" #"," #(Integer. %))))
+
+(def input-1202 
+  (into (conj (subvec input 0 1) 12 2) (subvec input 3))
+  )
 
 ;test
 (use 'clojure.test)
@@ -35,6 +39,7 @@
     (testing "multiplication"
       (is (= [2,3,0,6,99] (exec-instruction [2,3,0,3,99] 0)) "write result inside instruction index")
       (is (= [2,4,4,5,99,9801] (exec-instruction [2,4,4,5,99,0] 0)) "write result after instruction index")
+      (is (= [9801,3,0,3,2,8,8,0,99,0] (exec-instruction [2,3,0,3,2,8,8,0,99,0] 4)) "write result before instruction index")
       )
     )
   (testing "two instructions"
@@ -46,25 +51,12 @@
   )
 
 (deftest test-exec-program
-  (is (= [30,1,1,4,2,5,6,0,99] (exec-program [1,1,1,4,99,5,6,0,99] 0)))
-  (is (= 12490719 (first (exec-program input 0))))
+  (is (= [30,1,1,4,2,5,6,0,99] (exec-program [1,1,1,4,99,5,6,0,99] 0)) "add then multiply")
+  (is (= [11,1,1,4,1,5,6,0,99] (exec-program [2,1,1,4,99,5,6,0,99] 0)) "multiply then add")
+  (is (= 12490719 (first (exec-program input-1202 0))) "sample input")
   )
 
 (with-junit-output
   (run-tests 'user)
 )
-
-(exec-instruction [1,0,0,0,99] 0)
-
-(exec-instruction [2,3,0,3,99] 0)
-
-(exec-instruction [2,4,4,5,99,0] 0)
-
-(exec-instruction [1,1,1,4,99,5,6,0,99] 0)
-
-(exec-instruction [1,1,1,4,2,5,6,0,99] 4)
-
-(exec-program [1,1,1,4,99,5,6,0,99] 0)
-
-(exec-program input 0)
 

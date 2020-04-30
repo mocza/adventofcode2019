a9eb8fd4b4cc04173267e787d451f36d73f9e08e
MOCHA <zsolt.mocza@gmail.com>
2020-04-30 01:13:34 +0100

    directions to coordinates

---
+++ clj/src/clj/day3_1.clj
@@ -0,0 +1,54 @@
+(ns day3-1)
+(use 'clojure.test)
+(use 'clojure.test.junit)
+
+;(ns-unmap 'day3-1 'to-coordinates)
+
+(defn collection-type [col] (let [listObject (type list)] {"class clojure.lang.PersistentList" :list} (str (type col))))
+
+(with-test
+  (defn direction-to-coordinates [from move-to]
+    (let [move-to-direction (str (first (take 1 (first move-to))))
+          move-to-length (Integer. (apply str  (drop 1 (first move-to))))
+          ;coord-list #(map #(list) (range %1 %2))
+          [x y] from
+          [step start end f] ({"R" [1 (inc x) (inc move-to-length) #(list % y)] "U" [1 (inc y) (inc move-to-length) #(list x %)] "D" [-1 (dec y) (- move-to-length) #(list x %)] "L" [-1 (dec x) (- move-to-length) #(list % y)]} move-to-direction)
+          ]
+      (do
+        (println (format "move-to-direction: %s, move-to-length: %s, start: %s, end: %s, step: %s" move-to-direction move-to-length start end step))
+        (map f (range start end step))
+        ;(if (= "R" move-to-direction) (list (list (inc (first from)) (first (next from)))))
+        )))
+  (is (= '((1, 0)) (direction-to-coordinates '(0, 0) '("R1"))) "go to right one step")
+  (is (= '((1, 0) (2, 0) (3, 0) (4, 0) (5, 0) (6, 0) (7, 0) (8, 0) (9, 0) (10, 0)) (direction-to-coordinates '(0, 0) '("R10"))) "go to right 10 steps")
+  (is (= '((0, 0)) (direction-to-coordinates '(1, 0) '("L1"))) "go left one step")
+  (is (= '((0, 1)) (direction-to-coordinates '(0, 0) '("U1"))) "go up one step")
+  (is (= '((0, 0)) (direction-to-coordinates '(0, 1) '("D1"))) "go down one step")
+  )
+
+(with-junit-output
+  (run-tests 'day3-1))
+
+;(direction-to-coordinates '(0, 0) '("R1"))
+
+
+;; (with-test
+;;   (defn to-coord [start directions] 
+;;     (map #(if (= "R" (take 1 %) ) directions)
+;;     )
+;;   (is (= '((0,0) (1,0)) (to-coord '(0,0) '("R1"))))
+;;   )
+
+(with-test
+   (defn closest-intersection [central-port, wire1, wire2]
+     ;(let [wire1-coords (take 1 (first wire1))] ())
+     (Integer. 3)
+     )
+   (is (= 1 (closest-intersection '(0, 0) '("R1") '("R1"))))  
+  ;(is (= 3 (closest-intersection (1, 1) ("R8","U5","L5","D3") ("U7","R6","D4","L4"))))  
+   )
+
+(with-junit-output
+    (run-tests 'day3-1))
+
+;(closest-intersection '(1, 1) '("R8","U5","L5","D3") '("U7","R6","D4","L4")) 
